dd2853c43e3a6d407b1bf3539f180b984891bf50
Darwin Bayas <dbayas@jbp.com.ec>
2026-04-15 09:23:09 -0500

    variables de entorno dev y produccion

--- src/app/assets/enviroment.ts
+++ src/app/assets/enviroment.ts
@@ -2,3 +2,31 @@ export const enviroment = {
     empresaNombre: 'James Brown Pharma',
     empresaIniciales: 'JBP'
 };
+
+// Desarrollo
+export const UrlServicesDevelopment = {
+    userUrl: 'http://jbpservices/api/user',
+    SocioNegocioUrl: 'http://localhost:53628/api/socioNegocio',
+    periodoUrl: 'http://jbpservices/api/periodo',
+    cuentaUrl: 'http://jbpservices/api/cuenta',
+    signalRUrl: 'http://localhost:5000/notify', // esto es solo para pruebas de signalR
+    promotickServiceHubUrl: 'http://localhost:5000/checkOrdersToPromotickBusinessService',
+    promotickBusinessServiceOrdersUrl: 'http://localhost:5000/api/PromotickBusinessServicesOrders',
+    retencionesServiceHubUrl: 'http://localhost:5000/StatusManager',
+    retencionesServiceUrl: 'http://localhost:5000/api/Retenciones',
+    autorizacionesSRIServiceUrl: 'http://jbpservices/api/autorizacionSRI',
+}
+
+// produccion
+export const UrlServicesProduction = {
+    userUrl: 'http://services.jbp.com.ec/api/user',
+    SocioNegocioUrl: 'http://services.jbp.com.ec/api/socioNegocio',
+    periodoUrl: 'http://services.jbp.com.ec/api/periodo',
+    cuentaUrl: 'http://services.jbp.com.ec/api/cuenta',
+    signalRUrl: 'http://services2.jbp.com.ec/notify', // esto es solo para pruebas de signalR
+    promotickServiceHubUrl: 'http://services2.jbp.com.ec/checkOrdersToPromotickBusinessService',
+    promotickBusinessServiceOrdersUrl: 'http://services2.jbp.com.ec/api/PromotickBusinessServicesOrders',
+    retencionesServiceHubUrl: 'http://services2.jbp.com.ec/StatusManager',
+    retencionesServiceUrl: 'http://services2.jbp.com.ec/api/Retenciones',
+    autorizacionesSRIServiceUrl: 'http://services.jbp.com.ec/api/autorizacionSRI',
+}
