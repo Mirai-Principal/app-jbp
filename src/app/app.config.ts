e291e7a59bc2aeb23420eb41f72a2fb3a56688a6
Darwin Bayas <dbayas@jbp.com.ec>
2026-05-08 07:54:02 -0500

    cambio de colores y correccion de datapicker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { routes } from './app.routes';
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 import { provideHttpClient } from '@angular/common/http';
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+
 import { AppTitleStrategy } from './core/strategies/title.strategy';
 
 //registramos los datos de localizacion para espaniol
@@ -19,7 +21,9 @@ export const appConfig: ApplicationConfig = {
     {
       provide: TitleStrategy,
       useClass: AppTitleStrategy
-    } //proveedor de estrategia de título
+    }, //proveedor de estrategia de título
+    provideNativeDateAdapter(), //proveedor de adaptador de fecha
+    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' } //proveedor de locale de fecha
 
   ]
 };
