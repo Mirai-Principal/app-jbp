4307770085dfb60a5eebdfab8b4c92c3648b7bd8
Darwin Bayas <dbayas@jbp.com.ec>
2026-09-16 09:10:48 -0500

    build

--- src/app/assets/conf.ts
+++ src/app/assets/conf.ts
@@ -1,4 +1,4 @@
-export const entorno: "services" | "dev" | "servicesnube" = "dev";
+export const entorno: "services" | "dev" | "servicesnube" = "services";
 export const backend_api = `/api-${entorno}`;
 // export const service_layer = 'http://192.168.57.1:3300/api/v1/';
 export const service_layer = 'http://localhost:3300/api/v1/';
